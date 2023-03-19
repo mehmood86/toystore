4b5d2f89044cbb404ca2a4f92a9fde578605d274
Mehmood Ghaffar <engr_mehmood86@hotmail.com>
2023-03-19 21:17:50 +0100

    database backup
    Some very basic raw data. This should be replaced with more meaningful dataset.

---
+++ Dump20230319.sql
@@ -0,0 +1,76 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: toystoredb
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `product_table`
+--
+
+DROP TABLE IF EXISTS `product_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_table` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `price` double NOT NULL,
+  `quantity` int NOT NULL,
+  `total_price` double NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_table`
+--
+
+LOCK TABLES `product_table` WRITE;
+/*!40000 ALTER TABLE `product_table` DISABLE KEYS */;
+INSERT INTO `product_table` VALUES (1,'Samsung Galaxy Tab S8',800,1,0),(2,'Samsung Galaxy Tab S7',650,1,0),(3,'Samsung Galaxy Tab S6',450,1,0),(4,'Ipad Pro 1',800,1,0),(5,'Ipad Pro 2',1200,1,0),(6,'Ipad Air 1 (excellent condition)',350,1,0),(52,'Bluetooth Keyboard 3.0',15,50,750),(102,'Bluetooth Mouse wireless',5,50,250),(152,'test data',50,10,500);
+/*!40000 ALTER TABLE `product_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_table_seq`
+--
+
+DROP TABLE IF EXISTS `product_table_seq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product_table_seq` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product_table_seq`
+--
+
+LOCK TABLES `product_table_seq` WRITE;
+/*!40000 ALTER TABLE `product_table_seq` DISABLE KEYS */;
+INSERT INTO `product_table_seq` VALUES (251);
+/*!40000 ALTER TABLE `product_table_seq` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-19 21:14:10
